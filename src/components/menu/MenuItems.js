3c378f22c5251b46ae414a607540fb9e58e4a074
dania8shaghouri <daniashaghouri@gmail.com>
2025-11-28 01:08:54 +0300

    Added image and style to menu

--- src/components/menu/MenuItems.js
+++ src/components/menu/MenuItems.js
@@ -1,20 +1,40 @@
 import ButtonMenu from "../Home/ui/ButtonMenu";
 export default function MenuItems({ item }) {
   return (
-    <div>
-      <img src={item.img} alt={item.title} />
+    <section>
+      <div className=" flex items-start gap-5">
+        <img
+          className="rounded-[var(--radius-24)]
+          "
+          src={item.img}
+          alt={item.title}
+        />
 
-      <h3>{item.title}</h3>
+        <div className="px-4">
+          <h3
+            className="text-[var(--white)]  
+          font-[var(--weight-regular)] 
+          leading-[var(--lineHeight-2)]"
+          >
+            {item.title}
+          </h3>
 
-      <h3>{item.price}</h3>
+          <h3
+            className="text-[var(--gold-crayola)] 
+          float-right my-2.5"
+          >
+            {item.price}
+          </h3>
 
-      <p>{item.description}</p>
-
-      <ButtonMenu
-        href="#"
-        text="order"
-        className={` duration-700 delay-700  `}
-      />
-    </div>
+          <p
+            className="text-[var(--quick-silver)]
+          leading-[var(--lineHeight-4)]"
+          >
+            {item.description}
+          </p>
+        </div>
+      </div>
+      <ButtonMenu href="#" text="order" />
+    </section>
   );
 }
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,9 +1,13 @@
 import MenuItems from "../components/menu/MenuItems";
 import { menuData } from "../data/menuData";
-
+// img
+import shape1 from "../assets/img/shape-4.png";
+import shape2 from "../assets/img/shape-5.png";
 function Menu() {
   return (
-    <section className="">
+    <section className="bg-[var(--eerie-black-1)]">
+      <img src={shape1} />
+      <img src={shape2} />
       {menuData.map((item, index) => (
         <MenuItems key={index} item={item} />
       ))}
